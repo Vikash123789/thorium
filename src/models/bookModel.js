f63600e46de8b3f858a22832fd62ea28760ac1d7
Vikash123789 <golu123789@gmail.com>
2022-03-01 03:20:23 +0530

    Assignment Done BookModel

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,27 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+const bookModel = new mongoose.Schema({
+    bookName: {
+        type: String,
+        unique: true,
+        required: true
     },
-    sales: {type: Number, default: 10}
+    authorName: String,
+    tags: [String],
+    totalPages: Number,
+    year: {
+        type: Number,
+        default: 2021
+    },
+
+    StockAvailable: Boolean,
+    prices: {
+        indianPrice : String,
+        europePrice: String,
+    }
+
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Book', bookModel) //users
 
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+// const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBook", BookController.createBook  )
-
-router.get("/getBooksData", BookController.getBooksData)
-
+router.get("/bookList", BookController.bookList   )
+router.post("/getBooksInYear", BookController.getBooksInYear)
+router.post("/getParticularBooks", BookController.getParticularBooks)
+router.get("/getXINRBooks", BookController.getXINRBooks)
+router.get("/getRandomBooks", BookController.getRandomBooks)
 module.exports = router;
+
+// Assignment :
+// Create a books collection in your DB ( using bookModel with following fields)- bookName( mandatory field), price containing Indian and european price, year ( should be 2021 if no year is provided) , tags array, authorName, totalPages , stockAvailable ( true false) 
+// create the following API’s (write logic in bookController and routes in routes):
+// createBook : to create a new entry..use this api to create 11+ entries in your collection
+// bookList : gives all the books- their bookName and authorName only 
+// getBooksInYear: takes year as input in post request and gives list of all books published that year
+// getParticularBooks:- (this is a good one, make sincere effort to solve this) take any input and use it as a condition to fetch books that satisfy that condition
+// e.g if body had { name: “hi”} then you would fetch the books with this name
+// if body had { year: 2020} then you would fetch the books in this year
+// hence the condition will differ based on what you input in the request body
+// getXINRBooks- request to return all books who have an Indian price tag of “100INR” or “200INR” or “500INR” 
+// getRandomBooks - returns books that are available in stock or have more than 500 pages 
