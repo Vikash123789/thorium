97f659c0f93453b876da90389a97a05819761c56
Vikash123789 <golu123789@gmail.com>
2022-03-05 01:06:31 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -2,20 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+let requestIp = require('ip');
+let DateTimeYear = require('date-and-time');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+ 
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+
+mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Vikash123-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
+.then( () => console.log("MongoDb is connected and MiddleWare function as loGGer Also Connected"))
 .catch ( err => console.log(err) )
 
-app.use('/', route);
 
+app.use (function(req,res,next){
+    let now = new Date();
+    let dateTimeNow = DateTimeYear.format(now, 'YYYY/MM/DD HH:mm:ss')
+    let url = req.url
+    let clientIp = requestIp.address() ;
+    console.log(dateTimeNow,clientIp,url)
+    next();
+})
+
+
+
+app.use('/', route);
+// app.Logger1('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
