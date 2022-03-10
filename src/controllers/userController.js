00b462606a00faaf20245b5b0263b4bc219d581c
Vikash123789 <golu123789@gmail.com>
2022-03-11 01:24:38 +0530

    Try Catch HTTPS status COde

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,51 +1,89 @@
+const res = require("express/lib/response");
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (req, res) {
-  let data = req.body;
-  let savedData = await userModel.create(data);
-  res.send({ msg: savedData });
-};
+  try {
+    let data = req.body;
+    let savedData = await userModel.create(data);
+    res.status(201).send({ msg: savedData });
+  }
+  catch (error) {
+    res.status(500).send({ err: error.message })
+  }
+}
 
 const loginUser = async function (req, res) {
-  let username = req.body.emailId;
-  let password = req.body.password;
-  let user = await userModel.findOne({ emailId: username, password: password });
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "Please Enter Valid Username or Password",
-    });
-  let token = jwt.sign({ userId: user._id.toString() }, "Facebook");
-  res.send({ status: true, msg: token });
-};
+  try {
+    let username = req.body.emailId;
+    let password = req.body.password;
+    let user = await userModel.findOne({ emailId: username, password: password });
+    if (!user)
+      return res.status(400).send({
+        status: false,
+        msg: "Please Enter Valid Username or Password",
+      });
+    let token = jwt.sign({ userId: user._id.toString() }, "Facebook");
+
+    res.status(201).send({ status: true, msg: "Succesfully LogedIn.Here is a access Token", token: token })//msg: token });
+  }
+  catch (err) {
+    res.status(500).send({ err: err.message })
+  }
+}
+
+
 
 const getUserData = async function (req, res) {
-  let userId = req.params.userId;
-  let user = await userModel.findById(userId);
-  res.send({ status: true, msg: user });
+  try {
+    let userId = req.params.userId;
+
+    let user = await userModel.findById(userId);
+    res.status(200).send({ status: true, msg: user });
+  } catch (error) {
+    res.status(500).send({ error: error.message })
+  }
+
 };
+
+
+
+
+
 
 const updateUser = async function (req, res) {
-  let userId = req.params.userId;
-  let data = req.body;
-  let UpToDate = await userModel.findByIdAndUpdate(
+  try {
+    let userId = req.params.userId;
+    let data = req.body;
+    let UpToDate = await userModel.findByIdAndUpdate(
+      { _id: userId },
+      { $set: data },
+      { new: true }
+    );
+    res.status(200).send({ status: true, msg: "Updated Data" });
+  }
+  catch (error) {
+    res.status(500).send({ error: error.message })
 
-    { _id: userId },
-    { $set: data },
-    { new: true }
-  );
-  res.send({ status: true, msg: "Updated Data" });
-};
+  };
+}
+
+
 
 const deleteData = async function (req, res) {
-  let userId = req.params.userId;
-  let deletedData = await userModel.findByIdAndUpdate(
-    { _id: userId },
-    { $set: { isDeleted: true } },
-    { new: true }
-  );
-  res.send({ status: true, msg: "User Deleted"});
+  try {
+    let userId = req.params.userId;
+    let deletedData = await userModel.findByIdAndUpdate(
+
+      { _id: userId },
+      { $set: { isDeleted: true } },
+      { new: true }
+    );
+    res.status(200).send({ status: true, msg: "User Deleted" });
+  }
+  catch (error) {
+    res.status(500).send({ error: error.message })
+  }
 };
 
 module.exports.createUser = createUser;
