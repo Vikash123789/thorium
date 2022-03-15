b78e83dc032b8967cd1cc99b1b72e2c48ea19a59
Vikash123789 <golu123789@gmail.com>
2022-03-15 16:09:07 +0530

    phase 1 completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const moment = require("moment");
 const { default: mongoose } = require('mongoose');
 const app = express();
 
@@ -8,13 +9,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Vikash123-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
+
+
+
+
 app.use('/', route);
+
+
 
 
 app.listen(process.env.PORT || 3000, function () {
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const allControllers = require("../controllers/allControllers");
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/authors", allControllers.createAuthor);
+router.post("/blogs", allControllers.createBlog);
+router.get("/getblogs", allControllers.getBlogs);
+router.put("/updateBlogs/:blogId", allControllers.updateBlog);
+router.delete("/deleteBlogs/:BlogId", allControllers.deleteBlog);
+router.delete("/deleteByAddress", allControllers.deleteByAddress);
 
 module.exports = router;
