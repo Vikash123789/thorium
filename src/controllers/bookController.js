a21793a5db401ba2f50ee7079add92b09a8c3cd8
fractal1111 <parthauto4@gmail.com>
2022-03-30 22:22:22 +0530

    Final

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -11,46 +11,82 @@ const reviewModel = require("../models/reviewModel")
 const createBooks = async (req, res) => {
   try {
     console.log(req)
-    
+
     const data = req.body;
-    
-    if (Object.keys(data).length == 0 || data == null){ return res.status(400).send({ status: false, msg: "No input provided by user", }); }
+
+    if (Object.keys(data).length == 0 || data == null) {
+      return res.status(400).send({ status: false, msg: "No input provided by user", });
+    }
+
+
     const { title, excerpt, userId, ISBN, category, subcategory, releasedAt } = data;
-    
-    
 
 
 
-    if (!validation.valid(title)) { return res.status(400).send({ status: false, msg: "No title provided by user", }) }
+
+
+    if (!validation.valid(title)) {
+      return res.status(400).send({ status: false, msg: "No title provided by user", })
+    }
+
     const existTitle = await bookModel.findOne({ title: title })
-    if (existTitle) { return res.status(400).send({ status: false, msg: " Title already exist", }) }
 
-    if (!validation.valid(excerpt)) { return res.status(400).send({ status: false, msg: "No excerpt provided by user", }) }
-    
-    if (!validation.valid(userId)) { return res.status(400).send({ status: false, msg: "No userId provided by user", }) }
-    if (!validation.isValidObjectId(userId)) { return res.status(400).send({ status: false, msg: "Not  Valid Object Id", }); }
+    if (existTitle) {
+      return res.status(400).send({ status: false, msg: " Title already exist", })
+    }
+
+
+    if (!validation.valid(excerpt)) {
+      return res.status(400).send({ status: false, msg: "No excerpt provided by user", })
+    }
+
+
+    if (!validation.valid(userId)) {
+      return res.status(400).send({ status: false, msg: "No userId provided by user", })
+    }
+
+    if (!validation.isValidObjectId(userId)) {
+      return res.status(400).send({ status: false, msg: "Not  Valid Object Id", });
+    }
+
     const user = await userModel.findById(userId)
-    if (!user) { return res.status(404).send({ Status: false, msg: "User with this id not found" }) }
+    if (!user) {
+      return res.status(404).send({ Status: false, msg: "User with this id not found" })
+    }
+
 
     if (!validation.valid(ISBN)) { return res.status(400).send({ status: false, msg: " No ISBN provided by user", }) }
-    if (! validation.isValidIsbn(ISBN)) {
-      return res.status(400).send({ status: false, message: "Enter  valid ISBN number " });
+    if (!validation.isValidIsbn(ISBN)) {
+      return res.status(400).send({ status: false, message: "Enter  valid 12 digit ISBN number " });
     }
+
     const existISBN = await bookModel.findOne({ ISBN: ISBN })
-    if (existISBN) { return res.status(400).send({ status: false, msg: " ISBN already exist", }) }
+    if (existISBN) {
+      return res.status(400).send({ status: false, msg: " ISBN already exist", })
+    }
 
 
 
-    if (!validation.valid(category)) { return res.status(400).send({ status: false, msg: "No category provided by user", }) }
-    if (!validation.valid(subcategory)) { return res.status(400).send({ status: false, msg: "No subcategory provided by user", }) }
+    if (!validation.valid(category)) {
+      return res.status(400).send({ status: false, msg: "No category provided by user", })
+    }
 
-    if (!validation.valid(releasedAt)) { return res.status(400).send({ Status: false, msg: "Please provide the realesdAt field and enter a date" }) }
-    if (! validation.isValidDateFormat(releasedAt)) { return res.status(400).send({ Status: false, msg: "Please enter the date in correct YYYY-MM-DD format" }) }
+    if (!validation.valid(subcategory)) {
+      return res.status(400).send({ status: false, msg: "No subcategory provided by user", })
+    }
 
-    
+    if (!validation.valid(releasedAt)) {
+      return res.status(400).send({ Status: false, msg: "Please provide the realesdAt field and enter a date" })
+    }
+
+    if (!validation.isValidDateFormat(releasedAt)) {
+      return res.status(400).send({ Status: false, msg: "Please enter the date in correct YYYY-MM-DD format" })
+    }
+
+
     if (userId == req.decodedToken.userId) {
 
-     const saveData = await bookModel.create(data)
+      const saveData = await bookModel.create(data)
       return res.status(201).send({ status: true, msg: saveData })
 
     } else { return res.status(403).send({ Status: false, msg: "User is not authorized" }) }
@@ -58,7 +94,7 @@ const createBooks = async (req, res) => {
 
 
   } catch (err) {
-    console.log(err);
+
     return res.status(500).send({ status: false, msg: err, });
   }
 };
@@ -68,6 +104,7 @@ const createBooks = async (req, res) => {
 //Get Books
 const getBooks = async (req, res) => {
   try {
+
     const data = req.query;
     const filter = { isDeleted: false }
     const { userId, category, subcategory } = data
@@ -79,22 +116,26 @@ const getBooks = async (req, res) => {
       filter['userId'] = userId
 
     }
+
     if (validation.valid(category)) {
       filter['category'] = category
     }
-  
+
+
     if (validation.valid(subcategory)) {
       filter['subcategory'] = subcategory
     }
-    
 
 
-    const findData = await bookModel.find(filter).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 }).sort({title:1})
-    if (findData.length == 0) { return res.status(404).send({ Status: false, msg: "No books with the respected query were found" }) }
+
+    const findData = await bookModel.find(filter).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 }).sort({ title: 1 })
+    if (findData.length == 0) {
+      return res.status(404).send({ Status: false, msg: "No books with the respected query were found" })
+    }
 
     return res.status(200).send({ Status: true, msg: "Book List", Data: findData })
 
-  } catch (err) { console.log(err); return res.status(500).send({ status: false, msg: err, }); }
+  } catch (err) { return res.status(500).send({ status: false, msg: err, }); }
 };
 
 
@@ -102,44 +143,61 @@ const getBooks = async (req, res) => {
 //Get Books By Id
 const getById = async (req, res) => {
   try {
+
     const id = req.params.bookId;
-    
-    if (!validation.isValidObjectId(id)){return res.status(400).send({status: false,msg: "Enter valid Book id",})}
 
-    const findId = await bookModel.findById( id).lean()
-    if(!findId){ return res.status(404).send({status:false,msg :"No book exist with this Book id"})}
-    if (findId.isDeleted == true){ return res.status(400).send({status:false,msg :"already deleted"})}
+    if (!validation.isValidObjectId(id)) {
+      return res.status(400).send({ status: false, msg: "Enter valid Book id", })
+    }
 
-   
-   let reviews = await reviewModel.find({bookId:id , isDeleted:false})
-  
-   
-   findId['reviewsData']=reviews
-   
-   
-    return res.status(200).send({status : true, msg :"Data list",Data :findId})
+    const findId = await bookModel.findById(id).lean()
+    if (!findId) {
+      return res.status(404).send({ status: false, msg: "No book found with this Book id" })
+    }
+
+    if (findId.isDeleted == true) {
+      return res.status(400).send({ status: false, msg: "The book has been  deleted" })
+    }
 
 
-  } catch (err) {console.log(err);return res.status(500).send({status: false,msg:err.message,});}
+    let reviews = await reviewModel.find({ bookId: id, isDeleted: false })
+
+
+    findId['reviewsData'] = reviews
+
+
+    return res.status(200).send({ status: true, msg: "Data list", Data: findId })
+
+
+  } catch (err) { return res.status(500).send({ status: false, msg: err.message, }); }
 };
 
 
 //Update Books
 const updateBooks = async (req, res) => {
   try {
-    const id = req.params.bookId;
-    if(!validation.isValidObjectId(id)){return res.status(400).send({Status:false, msg:"Please enter valid id"})}
-    
 
-    if (Object.keys(req.body).length == 0 || req.body == null) { return res.status(400).send({ Status: false, msg: "Please provide input" }) }
-   
+    const id = req.params.bookId;
+
+    if (!validation.isValidObjectId(id)) { return res.status(400).send({ Status: false, msg: "Please enter valid id" }) }
+
+
+    if (Object.keys(req.body).length == 0 || req.body == null) {
+      return res.status(400).send({ Status: false, msg: "Please provide input" })
+    }
+
     const findId = await bookModel.findById(id)
-    if (!findId) { return res.status(404).send({ status: false, msg: "No book with the given id exists", }) }
-    if (findId.isDeleted == true) { return res.status(400).send({ Status: false, msg: "The requested book has been deleted" }) }
+
+    if (!findId) {
+      return res.status(404).send({ status: false, msg: "No book with the given id exists", })
+    }
+    if (findId.isDeleted == true) {
+      return res.status(400).send({ Status: false, msg: "The requested book has been deleted" })
+    }
 
     if (findId.userId == req.decodedToken.userId) {
-     
-       const { title, excerpt, ISBN, releasedAt } = req.body /////////
+
+      const { title, excerpt, ISBN, releasedAt } = req.body
 
       const filter = {}
 
@@ -151,7 +209,7 @@ const updateBooks = async (req, res) => {
       }
 
       if (validation.valid(ISBN)) {
-        if (! validation.isValidIsbn(ISBN)) { return res.status(400).send({ Status: false, msg: "Please entr a valid ISBN" }) }
+        if (!validation.isValidIsbn(ISBN)) { return res.status(400).send({ Status: false, msg: "Please entr a valid ISBN" }) }
         let dupliIsbn = await bookModel.findOne({ ISBN: ISBN })
         if (dupliIsbn) { return res.status(400).send({ Status: false, msg: "This ISBN already exists. Please enter a different ISBN" }) }
         filter["ISBN"] = ISBN
@@ -162,14 +220,14 @@ const updateBooks = async (req, res) => {
       }
 
       if (validation.valid(releasedAt)) {
-        if (! validation.isValidDateFormat(releasedAt)) { return res.status(400).send({ Status: false, msg: "Please enter the date in correct YYYY-MM-DD format" }) }
+        if (!validation.isValidDateFormat(releasedAt)) { return res.status(400).send({ Status: false, msg: "Please enter the date in correct YYYY-MM-DD format" }) }
 
         filter["releasedAt"] = releasedAt
       }
-console.log(filter)
+      console.log(filter)
 
 
-      const updateData = await bookModel.findOneAndUpdate({ _id: id}, {
+      const updateData = await bookModel.findOneAndUpdate({ _id: id }, {
         $set: filter
       }, { new: true })
 
@@ -187,11 +245,18 @@ const deleteBooks = async (req, res) => {
 
   try {
     let id = req.params.bookId
-    if(!validation.isValidObjectId(id)){return res.status(400).send({Status:false, msg:"Please enter a valid id"})}
+    if (!validation.isValidObjectId(id)) {
+      return res.status(400).send({ Status: false, msg: "Please enter a valid id" })
+    }
+
     let book = await bookModel.findById(id)
-    
-    if (!book) { return res.status(404).send({ Status: false, msg: "No book found" }) }
-    if (book.isDeleted === true) { return res.status(400).send({ Status: false, msg: "The requeste book has alredy been deleteed" }) }
+
+    if (!book) {
+      return res.status(404).send({ Status: false, msg: "No book with this id found" })
+    }
+    if (book.isDeleted === true) {
+      return res.status(400).send({ Status: false, msg: "The requeste book has alredy been deleteed" })
+    }
     if (book.userId == req.decodedToken.userId) {
 
       let deletedBook = await bookModel.findOneAndUpdate({ _id: id }, { isDeleted: true, deletedAt: Date.now() }, { new: true })
@@ -201,7 +266,7 @@ const deleteBooks = async (req, res) => {
 
 
   } catch (err) { return res.status(500).send({ Status: false, msg: err.message }) }
-  //if we delte a book therefor there review should also be deleted
+
 
 
 
@@ -217,4 +282,4 @@ module.exports = {
   deleteBooks,
 };
 
-//module.exports.createBooks = createBooks
+
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,36 +12,57 @@ const registerUser = async (req, res) => {
             return res.status(400).send({ status: false, msg: "No input" })
         }
         const { title, name, phone, email, password } = data;
-        if (!validation.valid(title)) { return res.status(400).send({ status: false, msg: "imput title" }) }
-       if(title != "Mr"&&"Misses"&&"Miss"){return res.status(400).send({Satus:false , msg:"Title can only be Mr , Misses and Miss" })}
-        if (!validation.valid(name)) { return res.status(400).send({ status: false, msg: "input name" }) }
-        if (!validation.valid(phone)) { return res.status(400).send({ status: false, msg: "input phone" }) }
+        if (!validation.valid(title)) {
+            return res.status(400).send({ status: false, msg: "No title provided by user" })
+        }
+
+        if (title != "Mr" && "Misses" && "Miss") {
+            return res.status(400).send({ Satus: false, msg: "Title can only be Mr , Misses and Miss" })
+        }
+
+        if (!validation.valid(name)) {
+            return res.status(400).send({ status: false, msg: "Name not provided by user" })
+        }
+
+        if (!validation.valid(phone)) {
+            return res.status(400).send({ status: false, msg: "Phone not provided by user" })
+        }
+
         if (!validation.isValidPhone(phone)) {
-            return res.status(400).send({ status: false, message: "Enter valid 10 digit indian mobile number " });
+            return res.status(400).send({ status: false, msg: "Enter valid 10 digit indian mobile number " });
         }
 
         const phoneExt = await userModel.findOne({ phone: data.phone })
-        if (phoneExt) { return res.status(400).send({ msg: "phone allready exists" }) }
+        if (phoneExt) {
+            return res.status(400).send({ msg: "phone allready exists" })
+        }
 
-        
-        if (!validation.valid(email)) { return res.status(400).send({ status: false, msg: "input email" }) }
-        if (! validation.isValidEmail(email)) {
-            return res.status(400).send({ status: false, message: "Enter  valid email address " });
+
+        if (!validation.valid(email)) {
+            return res.status(400).send({ status: false, msg: "No email provided by user" })
+        }
+        if (!validation.isValidEmail(email)) {
+            return res.status(400).send({ status: false, msg: "Enter a valid email address " });
         }
         const emailExt = await userModel.findOne({ email: data.email })
-        if (emailExt) { return res.status(400).send({ msg: " Email already exists " }) }
+        if (emailExt) {
+            return res.status(400).send({ msg: " Email already exists " })
+        }
 
-        if (!validation.valid(password)) 
-        { return res.status(400).send({ status: false, msg: "input password" }) }
-               if (password.length < 5 || password.length > 15) { return res.status(400).send({ msg: "Password minimum length is 5 and maximum length is 15" }) }
-       
+        if (!validation.valid(password)) {
+            return res.status(400).send({ status: false, msg: "No password provided by user" })
+        }
+        if (password.length < 5 || password.length > 15) {
+            return res.status(400).send({ msg: "Password minimum length is 5 and maximum length is 15" })
+        }
+
 
         let saveData = await userModel.create(data);
         return res.status(201).send({ status: true, msg: saveData })
     }
     catch (error) {
-        console.log(error)
-        return res.status(500).send({ status: false, msg: error });
+        
+        return res.status(500).send({ status: false, msg: error.message });
     }
 }
 
@@ -50,18 +71,28 @@ const loginUser = async (req, res) => {
     try {
         Data = req.body
 
-        if (Object.keys(Data).length == 0) { return res.status(400).send({ status: false, msg: "Please provide the input" }) }
+        if (Object.keys(Data).length == 0) {
+            return res.status(400).send({ status: false, msg: "Please provide the input" })
+        }
+
         const { email, password } = Data;
 
 
-        if (!validation.valid(email)) { return res.status(400).send({ status: false, msg: "Insert email" }) }
-        if (! validation.isValidEmail(email)) {
-            return res.status(400).send({ status: false, message: "Enter  valid email address " });
+        if (!validation.valid(email)) {
+            return res.status(400).send({ status: false, msg: "Insert email" })
         }
-        if (!validation.valid(password)) { return res.status(400).send({ status: false, msg: "Insert Password" }) }
+
+        if (!validation.isValidEmail(email)) {
+            return res.status(400).send({ status: false, msg: "Enter  valid email address " });
+        }
+        if (!validation.valid(password)) {
+            return res.status(400).send({ status: false, msg: "Insert Password" })
+        }
 
         const findUser = await userModel.findOne({ email: email, password: password })
-        if (!findUser) { return res.status(404).send({ status: false, msg: "No user with this email or password was found found" }) }
+        if (!findUser) {
+            return res.status(404).send({ status: false, msg: "No user with this email or password was found found" })
+        }
 
         const token = jwt.sign({
             userId: findUser._id,
@@ -69,12 +100,12 @@ const loginUser = async (req, res) => {
         }, "Project-Three", { expiresIn: "24h" }
 
         );
-        
-        return res.status(200).send({ status: true, msg: "Successful Login", Token:token })
+
+        return res.status(200).send({ status: true, msg: "Successful Login", Token: token })
     }
     catch (err) {
-        console.log(err)
-        return res.status(500).send({ status: false, msg: err })
+       
+        return res.status(500).send({ status: false, msg: err.message })
     }
 }
 
